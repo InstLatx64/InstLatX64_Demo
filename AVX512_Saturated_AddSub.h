62d774c4df54b990a6f5ebcd357062ee4683d17c
InstLatx64 <instlatx64@gmail.com>
2023-02-16 11:45:59 +0100

    Fix: missing _mm512_adds/subs_Zen4_epi/epu/32/64  from header
    -- Fix: missing _mm512_adds/subs_Zen4_epi/epu/32/64  from header

--- AVX512_Saturated_AddSub.h
+++ AVX512_Saturated_AddSub.h
@@ -1,13 +1,33 @@
 #pragma once
 
-__m512i _mm512_adds_epi32(__m512i x1, __m512i x2);
-__m512i _mm512_adds_epi64(__m512i x1, __m512i x2);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-__m512i _mm512_subs_epi32(__m512i x1, __m512i x2);
-__m512i _mm512_subs_epi64(__m512i x1, __m512i x2);
+__m512i _mm512_adds_epi32(__m512i a, __m512i b);
+__m512i _mm512_adds_epi64(__m512i a, __m512i b);
 
-__m512i _mm512_adds_epu32(__m512i x1, __m512i x2);
-__m512i _mm512_adds_epu64(__m512i x1, __m512i x2);
+__m512i _mm512_subs_epi32(__m512i a, __m512i b);
+__m512i _mm512_subs_epi64(__m512i a, __m512i b);
 
-__m512i _mm512_subs_epu32(__m512i x1, __m512i x2);
-__m512i _mm512_subs_epu64(__m512i x1, __m512i x2);
+__m512i _mm512_adds_epu32(__m512i a, __m512i b);
+__m512i _mm512_adds_epu64(__m512i a, __m512i b);
+
+__m512i _mm512_subs_epu32(__m512i a, __m512i b);
+__m512i _mm512_subs_epu64(__m512i a, __m512i b);
+
+__m512i _mm512_adds_Zen4_epi32(__m512i a, __m512i b);
+__m512i _mm512_adds_Zen4_epi64(__m512i a, __m512i b);
+
+__m512i _mm512_subs_Zen4_epi32(__m512i a, __m512i b);
+__m512i _mm512_subs_Zen4_epi64(__m512i a, __m512i b);
+
+__m512i _mm512_adds_Zen4_epu32(__m512i a, __m512i b);
+__m512i _mm512_adds_Zen4_epu64(__m512i a, __m512i b);
+
+__m512i _mm512_subs_Zen4_epu32(__m512i a, __m512i b);
+__m512i _mm512_subs_Zen4_epu64(__m512i a, __m512i b);
+
+#ifdef __cplusplus
+}
+#endif
